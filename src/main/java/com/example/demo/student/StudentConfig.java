6d78d9ccb07f65bc29e37c07e060ccacc1828e2a
namnd <namnguyennd7428@gmail.com>
2022-04-18 15:58:55 +0700

    add to github

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student nam = new Student(
+                    "Nam",
+                    "nam@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21
+            );
+            Student huy = new Student(
+                    "Huy",
+                    "huy@gmail.com",
+                    LocalDate.of(1993, Month.JANUARY, 5),
+                    29
+            );
+
+            repository.saveAll(List.of(nam, huy));
+        };
+    }
+}
