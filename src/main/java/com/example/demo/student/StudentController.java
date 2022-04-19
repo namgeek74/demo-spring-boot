c6a1c5d3e519cbde876490f4c567493784eb6cc0
namnd <namnguyennd7428@gmail.com>
2022-04-19 15:06:30 +0700

    create new api for CRUD with postgresql database

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,20 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudent() {
         return  studentService.getStudent();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PostMapping(path = "/edit")
+    public void editStudent(@RequestBody Student student) {
+        studentService.editStudent(student);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,10 +2,12 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,5 +20,41 @@ public class StudentService {
 
     public List<Student> getStudent() {
         return  studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentByEmail.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+
+        if (!exists) {
+            throw new IllegalStateException("this student does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void editStudent(Student student) {
+        Student currentStudent = studentRepository
+                .findById(student.getId())
+                .orElseThrow(
+                        () -> new IllegalStateException("this student does not exist")
+                );
+        if (student.getName() != null) {
+            currentStudent.setName(student.getName());
+        }
+        if (student.getEmail() != null) {
+            currentStudent.setEmail(student.getEmail());
+        }
+        if (student.getAge() != null) {
+            currentStudent.setAge(student.getAge());
+        }
     }
 }
